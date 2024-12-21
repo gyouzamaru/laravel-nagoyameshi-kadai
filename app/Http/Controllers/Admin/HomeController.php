4b52b75360008ad9cfa5cfe43baa28a3828fd6b3
gouzamaru <ami1225.2000@gmail.com>
2024-12-22 06:37:58 +0900

    管理者側のトップページ

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\User;
+use App\Models\Restaurant;
+use App\Models\Reservation;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
  public function index() {
-    return view('admin.home');
+
+   $total_users = User::Count();
+
+   $total_premium_users = DB::table('subscriptions')
+                          ->where('stripe_status', 'active')
+                          ->count();
+   
+   $total_free_users = $total_users - $total_premium_users;
+
+   $total_restaurants = Restaurant::Count();
+
+   $total_reservations = Reservation::Count();
+
+   $sales_for_this_month = 300 *	$total_premium_users;
+
+    return view('admin.home', compact('total_users', 'total_premium_users', 'total_free_users', 'total_restaurants', 'total_reservations', 'sales_for_this_month'));
  }
  
 }
